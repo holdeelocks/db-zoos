e38e90dbf62000e63476496cf569132eee3ad501
Holden B <holden.bucher.92@gmail.com>
2019-01-15 13:15:43 -0600

    endpoints twerking

--- index.js
+++ index.js
@@ -11,26 +11,70 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
-server.get("/", (req, res) => {
-  res.status(200).send("We made it fam");
-});
-
 server.get("/api/zoos", (req, res) => {
   db("zoos")
     .then(zoos => res.status(200).json(zoos))
     .catch(err => res.status(500).json(err));
 });
 
-server.get("/api/zoos/:id", (req, res) => {});
+server.get("/api/zoos/:id", (req, res) => {
+  const { id } = req.params;
+  db("zoos")
+    .where({ id })
+    .then(zoo => {
+      if (zoo.length > 0) {
+        res.status(200).json(zoo);
+      } else {
+        res.status(404).json({ error: "A zoo with that ID was not found" });
+      }
+    })
+    .catch(err => res.status(500).json(err));
+});
 
 server.post("/api/zoos", (req, res) => {
   const { name } = req.body;
   if (!name) res.status(400).send({ error: "Please include the name field" });
+  db("zoos")
+    .insert(req.body)
+    .then(ids => {
+      db("zoos")
+        .where({ id: ids[0] })
+        .then(zoo => res.status(201).json(zoo));
+    })
+    .catch(err => res.status(500).json(err));
 });
 
-server.put();
+server.put("/api/zoos/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
 
-server.delete();
+  db("zoos")
+    .where({ id })
+    .update(changes)
+    .then(count => {
+      if (count) {
+        res.status(200).json(count);
+      } else {
+        res.status(404).json({ error: "A zoo with that id was not found" });
+      }
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.delete("/api/zoos/:id", (req, res) => {
+  const { id } = req.params;
+  db("zoos")
+    .where({ id })
+    .del()
+    .then(count => {
+      if (count) {
+        res.status(200).json(count);
+      } else {
+        res.status(404).json({ error: "An entry with that id was not found" });
+      }
+    })
+    .catch(err => res.status(500).json(err));
+});
 
 const port = 3300;
 server.listen(port, function() {
