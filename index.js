007eda9f7ffe408cd7e925d06580218732464aaf
Holden B <holden.bucher.92@gmail.com>
2019-01-15 13:48:20 -0600

    bear routes working (stretch)

--- index.js
+++ index.js
@@ -76,6 +76,85 @@ server.delete("/api/zoos/:id", (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
+//
+//
+////// BEARS ENDPOINTS (STRETCH)
+//
+//
+
+server.get("/api/bears", (req, res) => {
+  db("bears")
+    .then(bears => res.status(200).json(bears))
+    .catch(err => res.status(500).json(err));
+});
+
+server.get("/api/bears/:id", (req, res) => {
+  const { id } = req.params;
+  db("bears")
+    .where({ id })
+    .then(bear => {
+      if (bear.length > 0) {
+        res.status(200).json(bear);
+      } else {
+        res.status(404).json({ error: "A bear with that ID does not exist" });
+      }
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.post("/api/bears", (req, res) => {
+  const { name } = req.body;
+  if (!name) res.status(400).json({ error: "Please include a name field" });
+
+  db("bears")
+    .insert(req.body)
+    .then(ids => {
+      db("bears")
+        .where({ id: ids[0] })
+        .then(bear => res.status(201).json(bear));
+    })
+    .catch(err => {
+      if ((err.errno = 19)) {
+        res.status(400).json({ error: "A bear with that name already exists" });
+      } else {
+        res.status(500).json(err);
+      }
+    });
+});
+
+server.put("/api/bears/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  db("bears")
+    .where({ id })
+    .update(changes)
+    .then(resp => {
+      if (resp) {
+        res.status(201).json(resp);
+      } else {
+        res.status(404).json({ error: "A bear with that id does not exist" });
+      }
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.delete("/api/bears/:id", (req, res) => {
+  const { id } = req.params;
+
+  db("bears")
+    .where({ id })
+    .del()
+    .then(count => {
+      if (count) {
+        res.status(200).json({ recordsDeleted: count });
+      } else {
+        res.status(404).json({ error: "A bear with that ID does not exist" });
+      }
+    })
+    .catch(err => res.status(500).json(err));
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
